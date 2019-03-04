043b437f7421bc257008666dd645b3cfbc09bbc1
Mike <mikeramzdesign@gmail.com>
2019-03-03 19:41:03 -0800

    graph DS

---
+++ Data_Structures/Graph_DS.js
@@ -0,0 +1,28 @@
+//=========================================
+//     GRAPH DATA STRUCTURE
+//=========================================
+
+/*
+NOTES:
+- Comprised of nodes (also known as vertices)
+- And a set of edges
+
+USEFUL FOR:
+- Describing Networks
+- Creating related nonhierachical data
+- Describing the relationships within one's faimly
+
+
+VERTICES:
+- also known as nodes
+- may be connected with other nodes
+
+EDGES:
+- When nodes connect with other nodes
+
+
+*/
+
+// Creating node factory function 
+
+function createNode(key)
